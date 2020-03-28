ade9a480562f56d1c828f81120aaf0ee21d063b0
Виктория <obuhovskayaviktoria@yandex.by>
2020-03-28 22:10:04 +0300

    Task30,31,32,33

---
+++ src/Lesson14/File.java
@@ -0,0 +1,29 @@
+package Lesson14;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class File {
+    public File(String s) {
+    }
+
+    public static void record(File file, String text) {
+        try (FileWriter writer = new FileWriter(String.valueOf(file))) {
+            writer.write(text);
+        } catch (IOException ex) {
+            System.out.println("No file");
+        }
+    }
+
+    public static void read(File file) {
+        try (FileReader reader = new FileReader(String.valueOf(file))) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException ex) {
+            System.out.println("File not read");
+        }
+    }
+}
+
---
+++ src/Lesson14/Task30.java
@@ -0,0 +1,18 @@
+package Lesson14;
+
+public class Task30 {
+    public static void main(String[] args) {
+        File file = new File("./text.txt");
+        String text = "Скоро весь сад, согретый солнцем, обласканный, ожил, и капли росы, как алмазы, засверкали на листьях.";
+        File.record(file, text);
+        File.read(file);
+        int countWord = 0;
+        int sentenceCount = 0;
+        String[] wordList = text.split("\\s");
+        countWord += wordList.length;
+        String[] sentenceList = text.split("[!?,.—:;]");
+        sentenceCount += sentenceList.length;
+        System.out.println("\nword count = " + countWord);
+        System.out.println("number of punctuation marks = " + sentenceCount);
+    }
+}
