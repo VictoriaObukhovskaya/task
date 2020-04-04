02d8a2cf3b3d884388c6f403520ff760d32ecb6e
VictoriaObukhovskaya <61082092+VictoriaObukhovskaya@users.noreply.github.com>
2020-04-04 18:22:11 +0300

    Delete Main.java

--- src/main/java/Lesson9/Task24/Main.java
+++
@@ -1,9 +0,0 @@
-package main.java.Lesson9.Task24;
-
-public class Main {
-    public static void main(String[] args) {
-        International.internationalbankcard();
-        Localcard.intrabankcard();
-        Virtual.virtualbankcard();
-    }
-}
