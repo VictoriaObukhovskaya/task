5e333f24aeb871589b1a81fc210455bf38813b0e
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-04 14:44:21 +0300

    Task5

--- src/main/java/Lesson2/Task5/Main.java
+++ src/main/java/Lesson2/Task5/Main.java
@@ -3,18 +3,19 @@ package main.java.Lesson2.Task5;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner num = new Scanner(System.in);
         int v;
         System.out.println(" ВВедите сумму ");
         v = num.nextInt();
-        if (v % 100 >= 11 && v % 100 <= 20) {
-            System.out.println(" рублей ");
-        } else if (v % 10 == 1) {
-            System.out.println(" рубль");
-        } else if (v % 10 == 2 || v % 10 == 3 || v % 10 == 4) {
-            System.out.println(" рубля");
-        } else {
+        if ( v % 100 >= 11 && v % 100 <= 20) {
+            System.out.println( " рублей "); }
+        else if ( v % 10 == 1 )
+        {
+            System.out.println(" рубль");}
+        else if ( v % 10 == 2 || v % 10 == 3 || v % 10 == 4 ) {
+            System.out.println( " рубля"); }
+        else {
             System.out.println(" рублей ");
         }
     }
