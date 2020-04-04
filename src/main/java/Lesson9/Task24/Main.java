4421995ad42edcec2d4e09e50adc5fa23069a564
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-04 15:43:17 +0300

    Task24

---
+++ src/main/java/Lesson9/Task24/Main.java
@@ -0,0 +1,9 @@
+package main.java.Lesson9.Task24;
+
+public class Main {
+    public static void main(String[] args) {
+        International.internationalbankcard();
+        Localcard.intrabankcard();
+        Virtual.virtualbankcard();
+    }
+}
