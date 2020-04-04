5fedb5dd38db6b7e4ae24cc977a8221f3ab8e1f5
VictoriaObukhovskaya <61082092+VictoriaObukhovskaya@users.noreply.github.com>
2020-04-04 18:22:06 +0300

    Delete Localcard.java

--- src/main/java/Lesson9/Task24/Localcard.java
+++
@@ -1,9 +0,0 @@
-package main.java.Lesson9.Task24;
-
-public class Localcard {
-    public static void intrabankcard() {
-        System.out.println("Локальные карты предназначены для совершения операций строго в системе определенного банка");
-    }
-}
-
-
