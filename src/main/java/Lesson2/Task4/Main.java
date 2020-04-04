f10a52e947977e9f61102ebecbc2a4abcf5cb1c9
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-04 13:43:12 +0300

    Task1,2,3,4,5,6,7

---
+++ src/main/java/Lesson2/Task4/Main.java
@@ -0,0 +1,23 @@
+package main.java.Lesson2.Task4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int r, a, b;
+        System.out.println(" ВВедите радиус круга ");
+        r = num.nextInt();
+        System.out.println(" ВВедите a ");
+        a = num.nextInt();
+        System.out.println(" ВВедите b ");
+        b = num.nextInt();
+        if (r * r >= (a * a + b * b) / 4)
+            System.out.println(" Можно");
+        else
+            System.out.println(" Нельзя");
+
+    }
+}
+
+
