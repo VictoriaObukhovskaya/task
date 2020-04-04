cd65daccf3439c439ba49f2b288f98347fd01cb7
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-04 14:12:15 +0300

    Task31

---
+++ src/main/java/Lesson13/Task31/Main.java
@@ -0,0 +1,25 @@
+package main.java.Lesson13.Task31;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("./text.txt");
+        String text = "1, 2, 3, 4, 5. Вышел зайчик погулять, запереть его забыли. 1, 2, 3, 4 ";
+        File.record(file, text);
+        File.read(file);
+        int sum = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i))) {
+                char number = text.charAt(i);
+                sum = sum + Character.getNumericValue(number);
+                System.out.print("\nnumber in text " + number);
+            }
+        }
+        System.out.print("\nsum of numbers " + sum);
+        StringBuilder sb = new StringBuilder();
+        text.chars().distinct().forEach(c -> sb.append((char) c));
+        System.out.print("\n" + sb);
+    }
+}
+
+
+
