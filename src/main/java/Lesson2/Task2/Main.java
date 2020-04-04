eec36eb89478e2a97ccf5ec29fba272820debda4
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-04 14:42:28 +0300

    Task2

--- src/main/java/Lesson2/Task2/Main.java
+++ src/main/java/Lesson2/Task2/Main.java
@@ -1,17 +1,17 @@
 package main.java.Lesson2.Task2;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         int s = 3700;
         int sec = s % 60;
         int m = (s - sec) / 60;
         int min = m % 60;
         int h = (m - min) / 60;
         int has = h % 60;
-        int st = (h - has) / 24;
+        int st = (h- has) / 24;
         int sutki = st % 24;
         int ndl = (st - sutki) / 7;
-        System.out.println(ndl + " недели " + sutki + " сутки " + has + " часов " + min + " минут " + sec + "секунд");
+        System.out.println( ndl + " недели " + sutki + " сутки " + has + " часов " + min + " минут " + sec + "секунд");
     }
 }
 
