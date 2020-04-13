88126455f088f5dc886004a964aadc19e04713e5
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-13 17:49:16 +0300

    задача про кассы

---
+++ src/main/java/Lesson13/Task32/File.java
@@ -0,0 +1,30 @@
+package main.java.Lesson13.Task32;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class File {
+    public File(String s) {
+    }
+
+    public static void record(File file, String text) {
+        try (FileWriter writer = new FileWriter(String.valueOf(file))) {
+            writer.write(text);
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println("No file");
+        }
+    }
+
+    public static void read(File file) {
+        try (FileReader reader = new FileReader(String.valueOf(file))) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException ex) {
+            System.out.println("File not read");
+        }
+    }
+}
+
---
+++ src/main/java/Lesson17/CashDesk.java
@@ -0,0 +1,8 @@
+package main.java.Lesson17;
+
+public class CashDesk {
+    public static int getCashDesk() {
+        return (int) (Math.random() * 2 + 1);
+    }
+}
+
