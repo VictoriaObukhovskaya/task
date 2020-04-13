7d3f83c4eff337b922a74ae9ca66c0b5948e270a
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-13 17:55:25 +0300

    Task32

--- src/main/java/Lesson13/Task32/Main.java
+++ src/main/java/Lesson13/Task32/Main.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "./number.txt";
+        String path = "./numbertext.txt";
         int[] array = new int[20];
         final Random random = new Random();
         for (int i = 0; i < array.length; ++i)
