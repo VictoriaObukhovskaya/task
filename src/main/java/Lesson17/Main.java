4e33041c5bf08cc06b857f0be7f279208478807a
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-16 19:03:41 +0300

    Провалидировать адрес электронного  кошелька

---
+++ src/main/java/Lesson17/Main.java
@@ -0,0 +1,15 @@
+package main.java.Lesson17;
+
+public class Main {
+    public static void main(String[] args) {
+        String address = "E134345810127";
+        String check = address.replaceAll(" ", "");
+        System.out.println(check);
+        boolean result = check.matches("[EDR]\\d{12}");
+        if (result) {
+            System.out.println("This is the right e-wallet address");
+        } else {
+            System.out.println("This is not right e-wallet address");
+        }
+    }
+}
