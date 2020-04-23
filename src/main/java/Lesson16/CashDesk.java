f4f8cae3aea576364aaf22d1fbb0ccd2ee397e93
Виктория <obuhovskayaviktoria@yandex.by>
2020-04-23 10:48:11 +0300

    кассы

--- src/main/java/Lesson16/CashDesk.java
+++ src/main/java/Lesson16/CashDesk.java
@@ -1,7 +1,34 @@
 package main.java.Lesson16;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
-public class CashDesk {
-    public static int getCashDesk() {
-        return (int) (Math.random() * 2 + 1);
+public class CashDesk extends Thread {
+    private String threadName;
+
+    public CashDesk(String threadName) {
+        this.threadName = threadName;
+    }
+
+    public void run() {
+        try {
+            for (int i = 1; i < 7; i++) { //номер покупателя для каждой кассы
+                List<String> products = Arrays.asList(
+                        "kefir, oranges, pasta",
+                        "baguette, cookies, rice",
+                        "apples, tomatoes, cabbage",
+                        "apricots, peanut, ham",
+                        "cheese, cocoa, melon");
+                Random rand = new Random();
+                String randomElement = products.get(rand.nextInt(products.size()));
+
+                System.out.println("At the cash desk " + threadName + " buyer " + i
+                        + " bought " + randomElement);
+                Thread.sleep(5000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("cash desk " + threadName + " completed");
     }
 }
--- src/main/java/Lesson16/Main.java
+++ src/main/java/Lesson16/Main.java
@@ -1,43 +1,17 @@
 package main.java.Lesson16;
 
-import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        List<String> products = Arrays.asList(
-                "kefir, oranges, pasta",
-                "baguette, cookies, rice",
-                "apples, tomatoes, cabbage",
-                "apricots, peanut, ham",
-                "cheese, cocoa, melon");
-        Random rand = new Random();
-        String randomElement1 = products.get(rand.nextInt(products.size()));
-        String randomElement2 = products.get(rand.nextInt(products.size()));
-        String randomElement3 = products.get(rand.nextInt(products.size()));
-        String randomElement4 = products.get(rand.nextInt(products.size()));
-        String randomElement5 = products.get(rand.nextInt(products.size()));
+    public static void main(String[] args) {
+        CashDesk cashDesk1 = new CashDesk("One");
+        CashDesk cashDesk2 = new CashDesk("Two");
+        CashDesk cashDesk3 = new CashDesk("Three");
 
-        Buyer buyer1 = new Buyer();
-        Buyer buyer2 = new Buyer();
-        Buyer buyer3 = new Buyer();
-        Buyer buyer4 = new Buyer();
-        Buyer buyer5 = new Buyer();
-
-        buyer1.start();
-        buyer1.join();
-        System.out.println(randomElement1);
-        buyer2.start();
-        buyer2.join();
-        System.out.println(randomElement2);
-        buyer3.start();
-        buyer3.join();
-        System.out.println(randomElement3);
-        buyer4.start();
-        buyer4.join();
-        System.out.println(randomElement4);
-        buyer5.start();
-        buyer5.join();
-        System.out.println(randomElement5);
+        cashDesk1.start();
+        cashDesk2.start();
+        cashDesk3.start();
     }
 }
+
+
+
 
