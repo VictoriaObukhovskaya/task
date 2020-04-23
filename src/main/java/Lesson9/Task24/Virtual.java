1199b93f2fe68046eadf938253042f3a2655d63e
VictoriaObukhovskaya <61082092+VictoriaObukhovskaya@users.noreply.github.com>
2020-04-23 11:53:39 +0300

    Delete Virtual.java

--- src/main/java/Lesson9/Task24/Virtual.java
+++
@@ -1,9 +0,0 @@
-package main.java.Lesson9.Task24;
-
-public class Virtual {
-    public static void virtualbankcard() {
-        System.out.println("Виртуальные карты предназначены для совершения операций исключительно через Интернет");
-    }
-}
-
-
